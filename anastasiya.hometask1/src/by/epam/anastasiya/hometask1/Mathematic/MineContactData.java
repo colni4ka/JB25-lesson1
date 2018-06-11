c79fddf4fcf5dab0fdce03af48741792c92b40a2
Anastasiya Berdnik <Anastasiya.Panasenka@thomsonreuters.com>
2018-06-11 22:55:52 +0300

    Hometask for the first lesson

---
+++ anastasiya.hometask1/src/by/epam/anastasiya/hometask1/Mathematic/MineContactData.java
@@ -0,0 +1,9 @@
+package by.epam.anastasiya.hometask1.Mathematic;
+
+public class MineContactData {
+ public static void main(String[] args) {
+	
+	 System.out.println ("Бердник Анастасия Александровна" + "\n" + "тел.+375 29 650 15 94"  + "\n" + "Адрес: Гамарника 2");
+ }
+
+}
---
+++ anastasiya.hometask1/src/by/epam/anastasiya/hometask1/Mathematic/SumForFour.java
@@ -0,0 +1,15 @@
+package by.epam.anastasiya.hometask1.Mathematic;
+
+public class SumForFour {
+	public static void main (String[] args) {
+		double a, b, c, d; //слагаемые
+		double sum; //сумма
+		a=2;
+		b=3;
+		c=4;
+		d=5;
+		sum=a+b+c+d;
+		System.out.println ("Сумма" + sum);
+	}
+
+}
