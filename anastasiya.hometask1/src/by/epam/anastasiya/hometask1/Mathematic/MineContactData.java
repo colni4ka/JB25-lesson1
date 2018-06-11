c57ebc3c102b0b3140a824d0d40d72c189ca79ee
Anastasiya Berdnik <Anastasiya.Panasenka@thomsonreuters.com>
2018-06-11 23:24:17 +0300

    Lesson1

--- anastasiya.hometask1/src/by/epam/anastasiya/hometask1/Mathematic/MineContactData.java
+++
@@ -1,9 +0,0 @@
-package by.epam.anastasiya.hometask1.Mathematic;
-
-public class MineContactData {
- public static void main(String[] args) {
-	
-	 System.out.println ("Бердник Анастасия Александровна" + "\n" + "тел.+375 29 650 15 94"  + "\n" + "Адрес: Гамарника 2");
- }
-
-}
